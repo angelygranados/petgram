75f3c8d08574fcdf64339f63ffe5c2496eea0c42
Angely Granados <angelinagranados@gmail.com>
2020-11-30 17:21:44 -0500

    adding helmet

--- src/App.js
+++ src/App.js
@@ -1,44 +1,39 @@
-import React from "react";
-import { Router } from "@reach/router";
+import React, { useContext } from "react";
 import { GlobalStyle } from "./styles/GlobalStyles";
 import Logo from "./components/Logo";
 import { NavBar } from "./components/NavBar";
+
 import { Home } from "./pages/Home";
 import { Detail } from "./pages/Detail";
-import { User } from "./pages/User";
 import { Favs } from "./pages/Favs";
+import { User } from "./pages/User";
+import { NotFound } from "./pages/NotFound";
 import { NotRegisteredUser } from "./pages/NotRegisteredUser";
-import Context from "./Context";
+
+import { Router, Redirect } from "@reach/router";
+import { Context } from "./Context";
 
 export const App = () => {
-  const urlParams = new window.URLSearchParams(window.location.search);
-  const detailId = urlParams.get("detail");
+  const { isAuth } = useContext(Context);
 
   return (
-    <>
+    <div>
       <GlobalStyle />
       <Logo />
       <Router>
+        <NotFound default />
         <Home path="/" />
-        <Home path="/pet/:id" />
-        <Detail path={"/detail/:detailId"} />
+        <Home path="/pet/:categoryId" />
+        <Detail path="/detail/:detailId" />
+        {!isAuth && <NotRegisteredUser path="/login" />}
+        {!isAuth && <Redirect noThrow from="/favs" to="/login" />}
+        {!isAuth && <Redirect noThrow from="/user" to="/login" />}
+
+        <Favs path="/favs" />
+        <User path="/user" />
       </Router>
-      <Context.Consumer>
-        {({ isAuth }) =>
-          isAuth ? (
-            <Router>
-              <User path={"/user"} />
-              <Favs path={"/favs"} />
-            </Router>
-          ) : (
-            <Router>
-              <NotRegisteredUser path={"/favs"} />
-              <NotRegisteredUser path={"/user"} />
-            </Router>
-          )
-        }
-      </Context.Consumer>
+
       <NavBar />
-    </>
+    </div>
   );
 };
--- src/Context.js
+++ src/Context.js
@@ -1,23 +1,29 @@
+import { navigate } from "@reach/router";
 import React, { createContext, useState } from "react";
-
 export const Context = createContext();
+
 const Provider = ({ children }) => {
   const [isAuth, setIsAuth] = useState(() => {
     return window.sessionStorage.getItem("token");
   });
+
   const value = {
     isAuth,
     activateAuth: (token) => {
       setIsAuth(true);
       window.sessionStorage.setItem("token", token);
+      navigate("/");
     },
     removeAuth: () => {
       setIsAuth(false);
       window.sessionStorage.removeItem("token");
+      navigate("/login");
     },
   };
+
   return <Context.Provider value={value}>{children}</Context.Provider>;
 };
+
 export default {
   Provider,
   Consumer: Context.Consumer,
